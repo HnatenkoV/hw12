6f81cce0888fe3ac73df07f79b13f75543b3d606
Hnatenkov <hnatenko.vadim3@gmail.com>
2023-01-21 20:22:03 +0200

    f2

--- script.js
+++ script.js
@@ -2,12 +2,13 @@ const  carTrafficRed = document.getElementById('t-red');
 const  carTrafficYellow = document.getElementById('t-yellow');
 const  carTrafficGreen = document.getElementById('t-green');
 const cnt = document.getElementById('counter');
+const btn = document.getElementById('buttonW');
 
 const circles = document.querySelectorAll('.light-circle');
 console.log(circles)
 let activeLight = 0;
 
-const startChangeLight = setInterval(() => {
+const startChangeLight = setInterval((event) => {
     changeLight();
 }, 5000)
 
@@ -24,28 +25,36 @@ function changeLight() {
 }
 
 let cntValue = 10;
+let onTimer;
 
-function handlerWalkingStart(event) {
+function stopTraffic() {
+    cntValue--;
+    cnt.innerHTML = cntValue;
+    if (cntValue === 0) {
+        setInterval((event) => {
+            changeLight();
+        }, 5000);
+    } else {
+        onTimer = setTimeout(stopTraffic, 1000)
+    }
+}
+
+
+
+function pressBtn() {
     clearInterval(startChangeLight);
+
 
     carTrafficRed.classList.add(carTrafficRed.getAttribute('activeColor'));
     carTrafficYellow.classList.remove(carTrafficYellow.getAttribute('activeColor'));
     carTrafficGreen.classList.remove(carTrafficGreen.getAttribute('activeColor'));
 
-        let timer = setInterval(() => {
-            cntValue -= 1;
-            cnt.innerHTML = cntValue;
-            if (cntValue === 0) {
-                clearInterval(timer);
-                cntValue = 10;
-            }
-
-            if (cntValue === 0) {
-                changeLight();
-                setInterval(() => {
-                    changeLight();
-                }, 5000)
-            }
-        }, 1000)
+    stopTraffic();
 }
+
+
+
+
+btn.addEventListener('click', pressBtn);
+
 
